e40f866c61396167a718eca1d8645b758fddd5d4
daniocen777 <danielochoa@outlook.com>
2024-09-26 17:07:39 -0500

    resolviendo

--- MarvelUniverseClean/app/src/main/java/com/danicode/marveluniverseclean/util/Constants.kt
+++ MarvelUniverseClean/app/src/main/java/com/danicode/marveluniverseclean/util/Constants.kt
@@ -8,8 +8,8 @@ class Constants {
 
     companion object {
         const val BASE_URL = "https://gateway.marvel.com"
-        const val API_KEY = "c68dad822ddaa174fd9d1ae822142ed1"
-        private const val PRIVATE_KEY = "77f1eed7081ee9fb4a9c61dbb5ba378e77681bd9"
+        const val API_KEY = ""
+        private const val PRIVATE_KEY = ""
         const val LIMIT = "20"
         val timeStamp = Timestamp(System.currentTimeMillis()).time.toString()
         fun hash(): String {
