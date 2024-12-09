e49ca58a51e94740d876503aa85f00595a8d73f8
daniocen777 <danielochoa@outlook.com>
2024-12-09 08:56:47 -0500

    Navegación: app bar, bottom. Drawer

---
+++ VirtualStore/app/src/main/java/com/danicode/virtualstore/seller/MainSellerActivity.kt
@@ -0,0 +1,12 @@
+package com.danicode.virtualstore.seller
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.danicode.virtualstore.R
+
+class MainSellerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_seller)
+    }
+}
